d866052e410e3f0d8954db71b67d63538f140eee
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2022-02-16 19:29:11 +0100

    Fixed Landing page

---
+++ script.js
@@ -0,0 +1,16 @@
+new fullpage('#fullpage',{
+    autoScrolling:true,
+    scrollHorizontally:true,
+    // licenseKey: '',
+    navigation: true,
+    anchors:['Num0', 'Num1', 'Num2', 'Num3'],
+    
+})
+
+//navigation menu click event
+const body = document.querySelector('body');
+const nav_icon = document.querySelector('#nav_icon');
+
+nav_icon.addEventListener('click', () => {
+    body.classList.toggle('nav_active');
+});
